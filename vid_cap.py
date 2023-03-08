f926afda9ddc8c40521c2d822fac489773143450
Amir <amirhossein.nayebiastaneh@gmail.com>
2023-03-08 11:34:14 +0100

    updates vid_cap.py

--- vid_cap.py
+++ vid_cap.py
@@ -2,32 +2,35 @@ import cv2
 import numpy as np
 import os
 import time
+import matplotlib.pyplot as plt
 
+verbose = False
+show_plot = False
+show_frames = True
 
-def ResizeAndDisplayFrame(win_name, frame, size):
-    resized = cv2.resize(frame, size)
+frames_dir = "frames"
+frame_count = 0
+
+def ResizeDisplayAndSaveFrame(win_name, frame, size = None):
+    if size is None:
+        resized = frame
+    else:
+        resized = cv2.resize(frame, size)
     cv2.imshow(win_name, resized)
+    cv2.imwrite(os.path.join(frames_dir, f"{win_name}-{frame_count}.bmp"),
+                    resized)
     return resized
 
 
-frames_dir = "frames"
+
 if not os.path.isdir(frames_dir):
     os.makedirs(frames_dir)
-
-# Define the codec and create a VideoWriter object
-codec = cv2.VideoWriter_fourcc(*'XVID')
-fps = 25
-frame_size_org = (960, 540)
-output_file_org = 'output_org.avi'
-
-out_org = cv2.VideoWriter(output_file_org, codec, fps, frame_size_org)
 
 bin_th = 5
 num_neurons = 100
 
 vid = cv2.VideoCapture(1)
 
-frame_count = 0
 previous_frame = None
 
 recording = False
@@ -48,22 +51,29 @@ while (True):
     org_w = int(org_w / 2)
     org_h = int(org_h / 2)
     frame = cv2.resize(frame, (org_w, org_h))
+    if show_frames:
+        ResizeDisplayAndSaveFrame("Main", frame)
+    
     frame = cv2.flip(frame, 1)
-    cv2.imshow("Main Image", frame)
+    if show_frames:
+        ResizeDisplayAndSaveFrame("Mirrored", frame)
 
     if ((frame_count % 2) == 0):
-
-        out_org.write(frame)
         # Resize the width to the number of neurons
         frame = cv2.resize(frame, (num_neurons, org_h), cv2.INTER_CUBIC)
-        ResizeAndDisplayFrame("Resized", frame, (org_w, org_h))
+        if show_frames:
+            ResizeDisplayAndSaveFrame("Resized", frame, (org_w, org_h))
 
         # Prepare image; grayscale and blur
         prepared_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if show_frames:
+            ResizeDisplayAndSaveFrame("Grayscale", prepared_frame, (org_w, org_h))
+
         prepared_frame = cv2.GaussianBlur(src=prepared_frame,
                                           ksize=(0, 0),
                                           sigmaX=3)
-        ResizeAndDisplayFrame("Blurred", prepared_frame, (org_w, org_h))
+        if show_frames:
+            ResizeDisplayAndSaveFrame("Blurred", prepared_frame, (org_w, org_h))
 
         # Set previous frame and continue if there is None
         if (previous_frame is None):
@@ -73,24 +83,23 @@ while (True):
 
         # Calculate difference and update previous frame
         diff_frame = cv2.absdiff(src1=previous_frame, src2=prepared_frame)
-        ResizeAndDisplayFrame("Diff Frame", diff_frame, (org_w, org_h))
+        if show_frames:
+            ResizeDisplayAndSaveFrame("Diff", diff_frame, (org_w, org_h))
 
         previous_frame = prepared_frame
 
         # Sum the columns of the matrix
         sum_cols = cv2.reduce(diff_frame, 0, cv2.REDUCE_AVG)
-        ResizeAndDisplayFrame("1D Diff", sum_cols, (org_w, 100))
+        if show_frames:
+            ResizeDisplayAndSaveFrame("1D Diff", sum_cols, (org_w, 100))
 
         thresh_frame = cv2.threshold(src=sum_cols,
                                      thresh=bin_th,
                                      maxval=255,
                                      type=cv2.THRESH_BINARY)[1]
 
-        thresh_frame_to_show = ResizeAndDisplayFrame("Bin", thresh_frame,
-                                                     (org_w, 100))
-
-        cv2.imwrite(os.path.join(frames_dir, f"{frame_count}.bmp"),
-                    thresh_frame_to_show)
+        if show_frames:
+            ResizeDisplayAndSaveFrame("1D Bin", thresh_frame, (org_w, 100))
 
         if not recording and 255 in thresh_frame:
             recording = True
@@ -100,10 +109,16 @@ while (True):
 
         if recording and not 255 in thresh_frame:
             recording = False
-            print('Spikes:', spikeArr)
-            print('Times:', timeArr)
+            if verbose:
+                print('Spikes:', spikeArr)
+                print('Times:', timeArr)
             # HERE WE SHOULD SEND spikeArr and timeArr TO THE NETWORK
-            # ...
+            if show_plot:
+                plt.scatter(timeArr, spikeArr)
+                plt.xlabel('Time (s)')
+                plt.ylabel('Neuron ID')
+                plt.grid(True)
+                plt.show()
 
         if recording:
             new_spikes = np.where(thresh_frame == 255)[1]
@@ -121,7 +136,6 @@ while (True):
 
 # After the loop release the cap object
 vid.release()
-out_org.release()
 
 # Destroy all the windows
 cv2.destroyAllWindows()
